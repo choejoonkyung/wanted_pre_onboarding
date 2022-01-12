45348e8803a732398b046f06813c1104b3c711b9
choejoonkyung <choejunkyung@gmail.com>
2022-01-13 00:21:21 +0900

    feat: CarouselItem css 적용

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import CarouselItem from "./components/Carousel/CarouselItem";
 function App() {
   return (
     <div className="App">
-      <CarouselItem></CarouselItem>
+      <CarouselItem
+        img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
+        title="유저 경험을 설계하라!"
+        subtitle="문제를 해결하는 프로덕트 디자인"
+        link="https://www.wanted.co.kr/events/wantedcon22"
+      ></CarouselItem>
     </div>
   );
 }
