babe667b5797b94468648dc976a5cb1fe93e6b57
choejoonkyung <choejunkyung@gmail.com>
2022-01-14 01:59:05 +0900

    fix: console 에러 해결

--- src/mocks.js
+++ src/mocks.js
@@ -1,47 +1,56 @@
 export const carousel = [
   {
+    id: 1,
     img: "https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg",
     title: "유저 경험을 설계하라!",
     subtitle: "문제를 해결하는 프로덕트 디자인",
     link: "https://www.wanted.co.kr/events/wantedcon22",
   },
+
   {
+    id: 2,
     img: "https://static.wanted.co.kr/images/banners/1452/be4ec643.jpg",
     title: "Git? GitHub?",
     subtitle: "협업 필수 도구 마스터하기",
     link: "https://www.wanted.co.kr/events/git_github",
   },
   {
+    id: 3,
     img: "https://static.wanted.co.kr/images/banners/1436/e2dd9445.jpg",
     title: "마케터를 위한 데이터",
     subtitle: "잘 나가는 마케터는 무엇이 다를까!?",
     link: "https://www.wanted.co.kr/events/livetalk36",
   },
   {
+    id: 4,
     img: "https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg",
     title: "해, 커리어 EP 02 공개",
     subtitle: "최종 발표를 위한 마지막 관문 2라운드의 승자는?",
     link: "https://www.youtube.com/watch?v=VLiACiB2lXw",
   },
   {
+    id: 5,
     img: "https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg",
     title: "개발자 성장 비결 공개!",
     subtitle: "글 쓰는 개발자들의 이야기",
     link: "https://www.wanted.co.kr/events/velog01",
   },
   {
+    id: 6,
     img: "https://static.wanted.co.kr/images/banners/1454/e504b006.jpg",
     title: "아는 만큼 보인다!",
     subtitle: "노동법 '채용' 시리즈",
     link: "https://www.wanted.co.kr/VLiACiB2lXw",
   },
   {
+    id: 7,
     img: "	https://static.wanted.co.kr/images/banners/1438/015566ac.jpg",
     title: "스타벅스 굿즈 좋아하세요?",
     subtitle: "사랑받는 디자인의 비밀",
     link: "https://www.wanted.co.kr/events/21_07_s06_b04",
   },
   {
+    id: 8,
     img: "https://static.wanted.co.kr/images/banners/1454/e504b006.jpg",
     title: "요즘 MD가 일하는 방법",
     subtitle: "실무자가 공개하는 MD 커리어의 모든 것!",
