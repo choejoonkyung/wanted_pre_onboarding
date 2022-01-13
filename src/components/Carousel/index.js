17e694839810d40e3343af251870b0383f65f87b
choejoonkyung <choejunkyung@gmail.com>
2022-01-13 18:56:14 +0900

    feat: Carousel 컴포넌트 뒤로가기 넘어가기 완성

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Carousel.module.css";
 
 function Carousel({ children }) {
+  const [active, setActive] = useState(0);
+
+  const controlActive = (newActive) => {
+    if (newActive < 0) {
+      setActive(0);
+      return;
+    }
+
+    if (newActive >= React.Children.count(children)) return;
+
+    setActive(newActive);
+  };
+
   return (
     <div className={styles.carousel}>
-      <div className={styles.inner} style={{ transform: "translateX(-200%)" }}>
+      <div
+        className={styles.inner}
+        style={{ transform: `translateX(-${active * 100}%)` }}
+      >
         {React.Children.map(children, (child, _) => {
           return (
             <div className={styles.innerItem} style={{ width: "100%" }}>
@@ -12,6 +28,23 @@ function Carousel({ children }) {
             </div>
           );
         })}
+      </div>
+
+      <div>
+        <button
+          onClick={() => {
+            controlActive(active - 1);
+          }}
+        >
+          prev
+        </button>
+        <button
+          onClick={() => {
+            controlActive(active + 1);
+          }}
+        >
+          next
+        </button>
       </div>
     </div>
   );
