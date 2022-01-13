428bc24d5d55556f581fb25554e85a8b3265f443
choejoonkyung <choejunkyung@gmail.com>
2022-01-14 00:38:15 +0900

    feat: Carousel 컴포넌트 무한반복 애니메이션

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,28 +2,48 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./Carousel.module.css";
 
 function Carousel({ children, autoMilisec = 5000 }) {
-  const [active, setActive] = useState(1);
+  const [active, setActive] = useState(0);
   const [itemWidth, setItemWidth] = useState(0);
   const [xDistance, setXdistance] = useState(0);
+  const [flag, setFlag] = useState(false);
   const ref = useRef(null);
 
-  const control = (newActive) => {
-    const lenght = Object.keys(children).length;
-    console.log(newActive);
-    if (newActive === -1) {
-      setActive(lenght - 1);
-      return;
-    }
-    if (newActive >= lenght) {
-      setActive(0);
-      return;
-    }
-    setActive(newActive);
+  const controlTransition = (newAtive) =>
+    new Promise((resolve, reject) => {
+      setFlag(true);
+      setActive(() => {
+        ref.current.style.transition = "transform 0.3s";
+        return newAtive;
+      });
+      setTimeout(() => {
+        ref.current.style.transition = "";
+        setFlag(false);
+      }, 300);
+      resolve();
+    });
+
+  const controlCarousel = async (newActive) => {
+    if (flag) return;
+    await controlTransition(newActive);
+
+    // active가 -1이거나 children 길이를 초과하게 될 때
+    // 실제 좌우에 데이터가 존재하는 위치로 이동 시키는 처리.
+    setTimeout(() => {
+      const lenght = Object.keys(children).length;
+      if (newActive == -1) {
+        setActive(lenght - 1);
+        return;
+      }
+      if (newActive >= lenght) {
+        setActive(0);
+        return;
+      }
+    }, 300);
   };
 
   useEffect(() => {
     const autoSlide = setInterval(() => {
-      control(active + 1);
+      controlCarousel(active + 1);
     }, autoMilisec);
     return () => {
       if (autoSlide) {
@@ -55,14 +75,25 @@ function Carousel({ children, autoMilisec = 5000 }) {
         ref={ref}
         style={{
           transform: `translateX(${
-            xDistance - itemWidth - active * itemWidth
+            xDistance - 2 * itemWidth - active * itemWidth
           }px)`,
         }}
       >
         <div className={`${styles.innerItem} inner-item`}>
-          {React.cloneElement(React.Children.toArray(children)[0], {
-            current: false,
-          })}
+          {React.cloneElement(
+            React.Children.toArray(children)[
+              React.Children.toArray(children).length - 2
+            ],
+            { current: false }
+          )}
+        </div>
+        <div className={`${styles.innerItem} inner-item`}>
+          {React.cloneElement(
+            React.Children.toArray(children)[
+              React.Children.toArray(children).length - 1
+            ],
+            { current: active === -1 }
+          )}
         </div>
         {React.Children.map(children, (child, index) => {
           return (
@@ -72,24 +103,28 @@ function Carousel({ children, autoMilisec = 5000 }) {
           );
         })}
         <div className={`${styles.innerItem} inner-item`}>
-          {React.cloneElement(
-            React.Children.toArray(children)[React.Children.count()],
-            { current: false }
-          )}
+          {React.cloneElement(React.Children.toArray(children)[0], {
+            current: active === Object.keys(children).length,
+          })}
+        </div>
+        <div className={`${styles.innerItem} inner-item`}>
+          {React.cloneElement(React.Children.toArray(children)[1], {
+            current: false,
+          })}
         </div>
       </div>
 
       <div>
         <button
           onClick={() => {
-            control(active - 1);
+            controlCarousel(active - 1);
           }}
         >
           prev
         </button>
         <button
           onClick={() => {
-            control(active + 1);
+            controlCarousel(active + 1);
           }}
         >
           next
