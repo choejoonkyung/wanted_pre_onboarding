35bab3f267d05ef63e13937251e3a32cc3235250
choejoonkyung <choejunkyung@gmail.com>
2022-01-14 01:38:56 +0900

    feat: Carousel 컴포넌트 화살표, 임시데이터 적용

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
 import Carousel from "./components/Carousel";
 import CarouselItem from "./components/Carousel/CarouselItem";
+import { carousel } from "./mocks";
 
 function App() {
   return (
     <div className="App">
       <Carousel>
-        <CarouselItem
-          img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-          title="유저 경험을 설계하라1!"
-          subtitle="문제를 해결하는 프로덕트 디자인"
-          link="https://www.wanted.co.kr/events/wantedcon22"
-        ></CarouselItem>
-        <CarouselItem
-          img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-          title="유저 경험을 설계하라2!"
-          subtitle="문제를 해결하는 프로덕트 디자인"
-          link="https://www.wanted.co.kr/events/wantedcon22"
-        ></CarouselItem>
-        <CarouselItem
-          img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-          title="유저 경험을 설계하라3!"
-          subtitle="문제를 해결하는 프로덕트 디자인"
-          link="https://www.wanted.co.kr/events/wantedcon22"
-        ></CarouselItem>
-        <CarouselItem
-          img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-          title="유저 경험을 설계하라4!"
-          subtitle="문제를 해결하는 프로덕트 디자인"
-          link="https://www.wanted.co.kr/events/wantedcon22"
-        ></CarouselItem>
-        <CarouselItem
-          img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-          title="유저 경험을 설계하라5!"
-          subtitle="문제를 해결하는 프로덕트 디자인"
-          link="https://www.wanted.co.kr/events/wantedcon22"
-        ></CarouselItem>
-        <CarouselItem
-          img="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-          title="유저 경험을 설계하라6!"
-          subtitle="문제를 해결하는 프로덕트 디자인"
-          link="https://www.wanted.co.kr/events/wantedcon22"
-        ></CarouselItem>
+        {carousel.map((item) => (
+          <CarouselItem
+            img={item.img}
+            title={item.title}
+            subtitle={item.subtitle}
+            link={item.link}
+          ></CarouselItem>
+        ))}
       </Carousel>
     </div>
   );
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -23,6 +23,7 @@ function Carousel({ children, autoMilisec = 5000 }) {
     });
 
   const controlCarousel = async (newActive) => {
+    // 다수의 입력 방지 플래그
     if (flag) return;
     await controlTransition(newActive);
 
@@ -114,22 +115,32 @@ function Carousel({ children, autoMilisec = 5000 }) {
         </div>
       </div>
 
-      <div>
-        <button
-          onClick={() => {
-            controlCarousel(active - 1);
-          }}
-        >
-          prev
-        </button>
-        <button
-          onClick={() => {
-            controlCarousel(active + 1);
-          }}
-        >
-          next
-        </button>
-      </div>
+      <button
+        className={styles.prevArrow}
+        onClick={() => {
+          controlCarousel(active - 1);
+        }}
+        style={{ left: xDistance - 60 }}
+      >
+        <span>
+          <svg class="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
+            <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+          </svg>
+        </span>
+      </button>
+      <button
+        className={styles.nextArrow}
+        onClick={() => {
+          controlCarousel(active + 1);
+        }}
+        style={{ right: xDistance - 60 }}
+      >
+        <span>
+          <svg class="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
+            <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+          </svg>
+        </span>
+      </button>
     </div>
   );
 }
